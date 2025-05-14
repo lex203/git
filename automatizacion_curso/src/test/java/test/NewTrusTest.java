fd2fc4be0ceec5c52805b5731667dda50f080555
lex203 <dpinedav@emeal.nttdata.com>
2025-05-14 09:39:19 -0500

    Se comienza a codificar Test

--- automatizacion_curso/src/test/java/test/NewTrusTest.java
+++ automatizacion_curso/src/test/java/test/NewTrusTest.java
@@ -1,4 +1,32 @@
 package test;
 
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.Open;
+import net.thucydides.core.annotations.findby.By;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NewTrusTest {
+    private WebDriver driver;
+    private Actor usuario;
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        usuario = Actor.named("Dahiana").whoCan(BrowseTheWeb.with(driver));
+    }
+
+    @Test
+    public void GuruRegister(){
+        usuario.attemptsTo(
+                Open.url("https://demo.guru99.com/test/newtours/"),
+                Click.on(By.name("userName")),
+                Enter.theValue("admin123").into(By.name("userName"))
+        );
+    }
 }
