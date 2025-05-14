bcc0f7a5774cccbf829a491c64e3e9df0f6a7373
lex203 <dpinedav@emeal.nttdata.com>
2025-05-14 10:17:00 -0500

    Se finaliza flujo

--- automatizacion_curso/src/test/java/test/NewTrusTest.java
+++ automatizacion_curso/src/test/java/test/NewTrusTest.java
@@ -4,12 +4,18 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.Hit;
 import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.questions.TextContent;
 import org.openqa.selenium.By;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+
 
 public class NewTrusTest {
     private WebDriver driver;
@@ -26,7 +32,18 @@ public class NewTrusTest {
         usuario.attemptsTo(
                 Open.url("https://demo.guru99.com/test/newtours/"),
                 Click.on(By.name("userName")),
-                Enter.theValue("admin123").into(By.name("userName"))
+                Enter.theValue("admin123").into(By.name("userName")),
+                Click.on(By.name("password")),
+                Enter.theValue("admin1234").into(By.name("password")),
+                Hit.the(Keys.ENTER).into(By.name("password"))
         );
+
+        usuario.attemptsTo(
+                Ensure.that(TextContent.of(By.xpath("//*[contains(text(), 'Login Successfully')]")))
+                        .contains("Login Successfully")
+        );
+
+
+        driver.quit();
     }
 }
